75df5f665f0bd532566dcd9fad31c5fa65ecd65b
kamepallipujitha <kamepallipujitha02@gmail.com>
2025-09-13 15:53:23 +0530

    first commit

---
+++ CSETRANSACTIONS5/src/test/java/com/example/demo/Csetransactions5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csetransactions5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
